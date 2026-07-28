ea85f04a5da7f3d0ea1b6a939187bc68e87019e4
Vitor Melo <vitormelodev@gmail.com>
2026-07-28 16:24:22 -0300

    Module 00 done

--- ex05/ft_water_reminder.py
+++
@@ -1,6 +0,0 @@
-def ft_water_reminder():
-    days = int(input("Days since last watering: "))
-    if days > 2:
-        print("Water the plants!")
-    elif days <= 2:
-        print("Plants are fine")
