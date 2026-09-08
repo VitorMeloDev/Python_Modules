0c55dbadb49ccf501a24cac623b1874033153414
Vitor Melo <vitormelodev@gmail.com>
2026-09-08 17:25:24 -0300

    feat: started module 04

---
+++ Module04/ex0/ft_ancient_text.py
@@ -0,0 +1,25 @@
+import sys
+from typing import IO
+
+def process_file(file_name: str) -> None:
+    print(f"Accessing file '{file_name}'\n---")
+
+    try:
+        file = open(file_name)
+        content = file.read()
+        print (content)
+    except FileNotFoundError as error:
+        print(error)
+    except OSError as error:
+        print(error)
+    file.close()
+    print("\n---")
+    print(f"File '{file_name}' closed")
+
+
+if __name__ == "__main__":
+    print("=== Cyber Archives Recovery ===")
+    if len(sys.argv) < 2:
+        print("Error: Enter a name file")
+    else:
+        process_file(sys.argv[1])
